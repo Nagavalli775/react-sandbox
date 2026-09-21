2cd6873d9a222f5be252b3c91f0f63cdd83e9401
Nagavalli775 <nagavallim166@gmail.com>
2026-09-21 22:34:24 +0530

    inception commit three

--- react-inception/inception-code/App.js
+++ react-inception/inception-code/App.js
@@ -1,6 +1,36 @@
-const heading = document.createElement("h1");
-heading.innerHTML = "Hello text from Javascript DOM";
+// js DOM code
 
-const root = document.getElementById("root");
+// const heading = document.createElement("h1");
+// heading.innerHTML = "Hello text from Javascript DOM";
 
-root.appendChild(heading);
+// const root = document.getElementById("root");
+
+// root.appendChild(heading);
+
+
+// react code - create element
+
+// const text = React.createElement("h1", {}, "Hello text from React js");
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(text);
+
+
+// react code - nested element
+
+const parent = React.createElement("div", {id: "parent"}, [
+	React.createElement("div", {id: "child1"}, [
+		React.createElement("h1", {}, "Heading 1"),
+		React.createElement("h2", {}, "Heading 2"),
+	]),
+
+	React.createElement("div", {id: "child2"}, [
+		React.createElement("h1", {}, "Heading 1"),
+		React.createElement("h2", {}, "Heading 2"),
+	]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
